6ce75572ded6e736368cc7510c59285845ee0f51
neilmale <neil.labasan.male@geeksnest.com>
2016-07-14 12:55:00 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# integer-input-directive
+# numeric-input-directive
+
+numeric parameters
+
+1. type: 'integer || decimal'
+2. min: '1 || 1.99'
+3. max: '10 || 10.99'
+
+examples:
+
+no parameter (no minimum, no maximum, integer(default))
+>`<input ng-model="age" type="text" numeric required/>`
+>`<input ng-model="age" type="text" numeric="" required/>`
+>
+
+with parameter (no minimum, no maximum, but type is whether integer(non decimal) or decimal)
+>`<input ng-model="age" type="text" numeric="{type: 'integer'}" required/>`
+>`<input ng-model="tax" type="text" numeric="{type: 'decimal'}" required/>`
+>
+
+with parameter (with minimum or maximum value)
+>`<input ng-model="age" type="text" numeric="{min: 1}" required/>`
+>`<input ng-model="tax" type="text" numeric="{max: 10}" required/>`
+>`<input ng-model="tax" type="text" numeric="{min:1, max: 10}" required/>`
+>
+
+do as you like
+>`<input ng-model="tax" type="text" numeric="{type: 'decimal', min: '1.5'}" required/>`
+>`<input ng-model="tax" type="text" numeric="{type: 'decimal', min: '1.5', max: '12.7'}" required/>`
+>`<input ng-model="age" type="text" numeric="{type: 'integer', min: 0, max: 10}" required/>`
+>
---
+++ numeric-directive.js
@@ -0,0 +1,90 @@
+.directive('numeric', function () {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        scope: {
+            numeric: '='
+        },
+        link: function (scope, elem, attr, ctrl) {
+            elem.bind('change', function() {
+                
+                var value, min, max;
+
+                if(scope.numeric !== undefined) {
+
+                    if(scope.numeric.hasOwnProperty('type')) {
+                        if(scope.numeric.type === 'integer') {
+                            value = isNaN(parseInt(ctrl.$viewValue)) ? 1 : parseInt(ctrl.$viewValue);
+                        } else if(scope.numeric.type === 'decimal') {
+                            value = isNaN(parseFloat(ctrl.$viewValue)) ? 1 : parseFloat(ctrl.$viewValue);
+                        } else {
+                            value = isNaN(parseInt(ctrl.$viewValue)) ? 1 : parseInt(ctrl.$viewValue);
+                            console.log('Warning: Number type is invalid. ("integer" or "decimal" only). Integer is default even without adding type');
+                        }
+                    } else {
+                        value = isNaN(parseInt(ctrl.$viewValue)) ? 1 : parseInt(ctrl.$viewValue);
+                    }
+
+                    if(scope.numeric.hasOwnProperty('min') && scope.numeric.hasOwnProperty('max')) {
+
+                        var minimum, maximum;
+
+                        if(scope.numeric.hasOwnProperty('type') && scope.numeric.type === 'decimal') {
+                            minimum = parseFloat(scope.numeric.min);
+                            maximum = parseFloat(scope.numeric.max);
+                        } else {
+                            minimum = parseInt(scope.numeric.min);
+                            maximum = parseInt(scope.numeric.max);
+                        }
+
+                        min = minimum > maximum ? maximum : minimum;
+                        max = maximum < minimum ? minimum : maximum;
+
+                        if(!isNaN(min) && !isNaN(max)) {
+
+                            if(value < min) {
+                                value = min;
+                            } else if(value > max) {
+                                value = max;
+                            }
+                            
+                        } else if (isNaN(min) && !isNaN(max)) {
+
+                            if(value > max) {
+                                value = max;
+                            }
+                            
+                        } else if (isNaN(max) && !isNaN(min)) {
+
+                            if(value < min) {
+                                value = min;
+                            }
+
+                        }
+
+                    } else if (scope.numeric.hasOwnProperty('min')) {
+
+                        min = parseInt(scope.numeric.min);
+
+                        if(!isNaN(min) && value < min) {
+                            value = min;
+                        }
+
+                    } else if (scope.numeric.hasOwnProperty('max')) {
+
+                        max = parseInt(scope.numeric.max);
+
+                        if(!isNaN(max) && value > max) {
+                            value = max;
+                        }
+
+                    }
+                }
+
+                ctrl.$setViewValue(value.toString());
+                ctrl.$render();
+
+            });
+        }
+    };
+});
